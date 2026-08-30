e403f3ee6dcfca305b1ba0edf3433a4f9f38cba7
aziz-arrasyid <azizvfydani12@gmail.com>
2026-08-30 17:19:07 +0700

    Add a player character

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -3,24 +3,28 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
-    public class CellsData
+    public class CellData
     {
         public bool Passable;
     }
 
-    private CellsData[,] m_BoardData;
+    private CellData[,] m_BoardData;
 
     private Tilemap m_Tilemap;
+    private Grid m_Grid;
 
     public int Width;
     public int Height;
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
+    public PlayerController Player;
 
     private void Start()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
-        m_BoardData = new CellsData[Width, Height];
+        m_Grid = GetComponentInChildren<Grid>();
+
+        m_BoardData = new CellData[Width, Height];
 
         for(int y = 0; y < Height; ++y)
         {
@@ -43,5 +47,23 @@ public class BoardManager : MonoBehaviour
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+
+        Player.Spawn(this, new Vector2Int(1, 1));
+    }
+
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if (cellIndex.x < 0 || cellIndex.x >= Width
+            || cellIndex.y < 0 || cellIndex.y >= Height)
+        {
+            return null;
+        }
+
+        return m_BoardData[cellIndex.x, cellIndex.y];
+    }
+
+    public Vector3 CellToWorld(Vector2Int cellIndex)
+    {
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
     }
 }
